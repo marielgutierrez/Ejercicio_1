13e48620f52a150a28b2b21ce2f1cebf00590af0
= <=>
2024-08-29 18:22:26 -0300

    actualizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,51 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./components/login/login.component";
+import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, LoginComponent,BienvenidoComponent, ErrorComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'clase-01';
+  constructor(){
+
+  }
+
+  edadUno : string = '';
+  edadDos : string = '';
+  promedio : number = 0;
+  suma : number = 0;
+
+  sumarEdades():number{
+    let num_uno = parseFloat(this.edadUno);
+    let num_dos = parseFloat(this.edadDos);
+
+
+    if (!isNaN(num_uno) && !isNaN(num_dos)) {
+      this.suma = num_uno + num_dos;
+    } else {
+      this.suma = 0;
+      alert("Por favor, ingrese numeros que sean válidos");
+    }
+    return this.suma;
+  }
+
+  promediarEdades():number{
+    this.promedio = this.sumarEdades() / 2;
+    return this.promedio;
+  }
+
+  limpiarInput(){
+    this.edadUno = '';
+    this.edadDos = '';
+    this.promedio = 0;
+    this.suma = 0;
+  }
+
 }
---
+++ src/app/components/bindeos/bindeos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bindeos',
+  standalone: true,
+  imports: [],
+  templateUrl: './bindeos.component.html',
+  styleUrl: './bindeos.component.css'
+})
+export class BindeosComponent {
+
+}
